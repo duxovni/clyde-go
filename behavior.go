f43bd13d622ba0e5d5d31921caf4247cffafbea0
Samuel M Dukhovni <dukhovni@mit.edu>
2016-12-21 18:53:43 -0500

    Prevent incorrect template-filling on fight regexp

--- behavior.go
+++ behavior.go
@@ -136,7 +136,7 @@ var story = StandardBehavior("tell me a story",
 		return fmt.Sprintf("Once upon a time, there was %s %s named %s who", stringutil.Article(job), job, sender)
 	})
 
-var fight = StandardBehavior("if (?P<fight1>.+) and (?P<fight2>.+) (fought|got in|were in|had)|between (?P<fight1>.+) and (?P<fight2>.+[^,\\?])(,|\\?|$| who| which| what)",
+var fight = StandardBehavior("if (?P<fight1>.+) and (?P<fight2>.+) (fought|duell?ed|got in|were in|had)|(who|which|what) .* between (?P<fight1>.+) and (?P<fight2>.+[^,\\?])(\\?|$)|between (?P<fight1>.+) and (?P<fight2>.+[^,\\?]),? (who|which|what)",
 	[]string{"fight1", "fight2"},
 	true,
 	func(c *Clyde, r zephyr.MessageReaderResult, kvs map[string]string) string {
